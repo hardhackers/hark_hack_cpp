dff7c5a558694bf664980e0333635cf01f9bf3d4
Yannick SIRJEAN <ysirjean@gmail.com>
2018-11-24 15:32:13 +0100

    corrections typo

--- src/ex.cpp
+++ src/ex.cpp
@@ -1,12 +1,16 @@
     #include <iostream>
-    #include <typeinfo>
-
+    
     int main ()
     {
-
-      std::cout << "Voici la taille de signed int : " << sizeof(signed) << std::endl;
-      std::cout << "Voici le type id de signed int : " << typeid(signed int).name() << std::endl;
-
+      int premier_int = 2147483647;
+      int deuxieme_int = 0;
+      int somme = premier_int + deuxieme_int;
+      std::cout << "Premier entier : " << premier_int << std::endl;
+      std::cout << "Deuxième entier : " << deuxieme_int << std::endl;
+      std::cout << "Somme : " << somme << std::endl;
+      deuxieme_int = 1;
+      somme = premier_int + deuxieme_int;
+      std::cout << "Somme déborde : " << somme << std::endl;
     }
 
 
