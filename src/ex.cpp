d243af2271ef65f5256085e0c172c38a9e93166b
Yannick SIRJEAN <ysirjean@gmail.com>
2018-11-24 16:03:51 +0100

    ajout du tableau climits

--- src/ex.cpp
+++ src/ex.cpp
@@ -1,16 +1,13 @@
     #include <iostream>
-    
+    #include <climits>
+
     int main ()
     {
-      int premier_int = 2147483647;
-      int deuxieme_int = 0;
-      int somme = premier_int + deuxieme_int;
-      std::cout << "Premier entier : " << premier_int << std::endl;
-      std::cout << "Deuxième entier : " << deuxieme_int << std::endl;
-      std::cout << "Somme : " << somme << std::endl;
-      deuxieme_int = 1;
-      somme = premier_int + deuxieme_int;
-      std::cout << "Somme déborde : " << somme << std::endl;
+
+      std::cout << "Voici la valeur max de unsigned long long int est : " << ULLONG_MAX << std::endl;
+
     }
+
+
 
 
