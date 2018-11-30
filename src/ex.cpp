81c74401be62aff6ce8360431a1f3c18868f4c96
Yannick SIRJEAN <ysirjean@gmail.com>
2018-11-30 23:23:30 +0100

    ajout réutilisation nom de variables

--- src/ex.cpp
+++ src/ex.cpp
@@ -1,12 +1,12 @@
     #include <iostream>
 
+    int mon_int1{5};
+    
     int main()
     {
-      int _int1{15};
-      std::cout << "Voici la somme : " << _int1 << std::endl;
-      //provoque une erreur
-      //int 1_int{15};
-      //idem
-      //std::cout << "Voici la somme : " << 1_int1 << std::endl;
-
-    } 
+      int mon_int1{4};
+      // les doubles "deux points" sont là pour identifier une variable globale
+      std::cout << "Voici l'entier 1 qui est une variable globale : " << ::mon_int1 << std::endl;
+      // la variable locale à la fonction
+      std::cout << "Voici l'entier 1 à l'intérieur de main : " << mon_int1 << std::endl;
+    }
