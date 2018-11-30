6dca7db284ea765dc2d7b788672e77ce3a99f147
Yannick SIRJEAN <ysirjean@gmail.com>
2018-11-30 23:46:27 +0100

    ajout précisions redéfinitions de variables

--- src/ex.cpp
+++ src/ex.cpp
@@ -4,9 +4,15 @@
     
     int main()
     {
+   
+      std::cout << "Voici l'entier 1 qui est une variable globale : " << mon_int1 << std::endl;
       int mon_int1{4};
-      // les doubles "deux points" sont là pour identifier une variable globale
-      std::cout << "Voici l'entier 1 qui est une variable globale : " << ::mon_int1 << std::endl;
-      // la variable locale à la fonction
       std::cout << "Voici l'entier 1 à l'intérieur de main : " << mon_int1 << std::endl;
+      //pas ok
+      int mon_int1{3};
+        {
+          //ok
+          int mon_int1{2};
+          std::cout << "Voici un autre entier 1 à l'intérieur d'un espace de nom inclus dans main : " << mon_int1 << std::endl;
+         }
     }
