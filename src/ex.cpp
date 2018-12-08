5c4d764df2f6328bdf7dca2ee05382be1f7f873d
Yannick SIRJEAN <ysirjean@gmail.com>
2018-12-08 17:15:22 +0100

    ajout de la partie sur les alias

--- src/ex.cpp
+++ src/ex.cpp
@@ -1,13 +1,10 @@
-    #include<iostream>
+    #include <iostream>
+    
+    using std::cout;
+    using std::endl;
 
     int main()
     {
-      int mon_entier{654444};
-      std::cout << "Affichage de l'entier : " << mon_entier << std::endl;
-      int* mon_pointeur = &mon_entier;
-      int mon_entier2 = *mon_pointeur;
-      std::cout << "Affichage de l'entier mis dans un entier : " << mon_entier2 << std::endl; 
-      char mon_char = *mon_pointeur;
-      int mon_entier3 = mon_char;
-      std::cout << "Affichage de l'entier mis dans un char : " << mon_entier3 << std::endl; 
-    }
+      int entier1{4};
+      cout << "Voici l'entier1 : " << entier1 << endl;
+     }
